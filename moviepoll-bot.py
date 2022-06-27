4d2e98bd3531a4a23ae2a6de911a30278c283e4a
jmateusms <jmateussan@gmail.com>
2022-06-27 17:38:23 -0300

    more webhook testing

--- moviepoll-bot.py
+++ moviepoll-bot.py
@@ -18,17 +18,14 @@ bot = telebot.TeleBot(TOKEN)
 
 server = Flask(__name__)
 
+bot.remove_webhook()
+bot.set_webhook(url='https://moviepoll-bot.herokuapp.com/' + TOKEN)
+
 @server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
+def webhook():
+    json_string = request.stream.read().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://moviepoll-bot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 # initialize bot memory
